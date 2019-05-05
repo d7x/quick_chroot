2eedee813b1bdf7064253dad4400e222aa9aff70
d7x <36901198+d7x@users.noreply.github.com>
2019-05-05 13:17:11 +0300

    Create quick_chroot.sh

---
+++ quick_chroot.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# define root filesystem
+export target_device=/dev/sdb3
+declare -a bind_targets=(/dev/pts /dev /proc /sys)
+
+# mount target system partitions
+for  i in ${bind_targets[@]}; do mount --bind $i /mnt$i && wait $!
+
+chroot /mnt
+
+grub-update
+grub-install
+
+# exit the chroot jail and unmount devices
+exit
+for  i in ${bind_targets[@]}; do umount /mnt$i
